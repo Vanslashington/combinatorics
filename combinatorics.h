286216ded731a0c3c08bf3e19d951564803ee6ba
David Vaughan <DavidRayVaughan00@gmail.com>
2015-12-20 10:48:48 -0800

    Create combinatorics static_resources class
    This is an uninstantiable class to hold static objects and variables
    that may be shared by combinatorial functions, such as Pascal's
    triangle.
    
    Pascal's triangle has been moved into this class.

--- combinatorics.h
+++ combinatorics.h
@@ -1,20 +1,52 @@
 #ifndef COMBINATORICS_H
 #define COMBINATORICS_H
 
+#include <vector>
+
 /**
- * @brief Returns the number of combinations of r objects from a set of n
- *        objects.
+ * @brief Uninstantiable class to hold static objects and variables needed
+ *        by combinatorial functions
  *
- * Returns the result of the typical nCr function, which is equal to:
- *  (n!)/((n-r)! * r!)
- * for all r <= n, and
- *  0
- * for all r > n
- *
- * @param n the integer number of objects in the set to choose from
- * @param r the integer number of objects to select from a set of n objects
- * @return the number of combinations of r objects from n
+ * This class will not have any public functions of its own, only outside
+ * friend functions that can make use of the variables and objects inside of
+ * the class.
  */
+class static_resources {
+ public:
+  /**
+   * @brief Returns the number of combinations of r objects from a set of n
+   *        objects.
+   *
+   * Returns the result of the typical nCr function, which is equal to:
+   *  (n!)/((n-r)! * r!)
+   * for all r <= n, and
+   *  0
+   * for all r > n
+   *
+   * @param n the integer number of objects in the set to choose from
+   * @param r the integer number of objects to select from a set of n objects
+   * @return the number of combinations of r objects from n
+   */
+  friend int combinations(int n, int r);
+
+ private:
+  static_resources(); // Private constructor; can't be instantiated
+
+  /// Create Pascal's triangle as a static 2D vector, for keeping
+  /// previously-computed values between function calls
+  static std::vector<std::vector<int> > pascal;
+  static int greatestPascal_n, greatestPascal_r;
+};
+
+// Pascal's triangle
+// Start at size 1x1;  0C0 == 1
+std::vector<std::vector<int> >
+static_resources::pascal(0+1, std::vector<int>(0+1, 1));
+
+int static_resources::greatestPascal_n = 0,
+    static_resources::greatestPascal_r = 0;
+
+// Combinatorial function prototypes
 int combinations(int n, int r);
 
 #include "combinatorics.tpp" // Template implementation file
--- combinatorics.tpp
+++ combinatorics.tpp
@@ -18,33 +18,28 @@ int combinations(int n, int r) {
   /// nCr = 0 for all r > n
   if(r > n) return 0;
 
-  // typedef's for brevity and readability
-  typedef int              T;
-  typedef std::vector<T>   vec;
-  typedef std::vector<vec> vec2d;
-
-  /// Create Pascal's triangle as a static 2D vector, because we want
-  /// previously-computed results to be saved for future calls of the function
-  static vec2d pascal(0+1, vec(0+1, 1)); // Start at size 1x1;  0C0 == 1
-  static int greatestComputed_n = 0, greatestComputed_r = 0;
+  // Aliases for brevity
+  std::vector<std::vector<int> >& pascal = static_resources::pascal;
+  int& greatestPascal_n = static_resources::greatestPascal_n;
+  int& greatestPascal_r = static_resources::greatestPascal_r;
 
   /// Expand the triangle "rectangularly" downward until we've computed nCr
-  if(r > greatestComputed_r) {
-    for(int i = 1; i <= greatestComputed_n; ++i) {
+  if(r > greatestPascal_r) {
+    for(int i = 1; i <= greatestPascal_n; ++i) {
       pascal[i].resize(std::min(i+1, r+1), 1);
-      for(int j = greatestComputed_r+1; j < i && j <= r; ++j)
+      for(int j = greatestPascal_r+1; j < i && j <= r; ++j)
         pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
     }
-    greatestComputed_r = r;
+    greatestPascal_r = r;
   }
-  if(n > greatestComputed_n) {
-    pascal.resize(n+1, vec());
-    for(int i = greatestComputed_n+1; i <= n; ++i) {
-      pascal[i].resize(std::min(i+1, greatestComputed_r+1), 1);
-      for(int j = 1; j < i && j <= greatestComputed_r; ++j)
+  if(n > greatestPascal_n) {
+    pascal.resize(n+1, std::vector<int>());
+    for(int i = greatestPascal_n+1; i <= n; ++i) {
+      pascal[i].resize(std::min(i+1, greatestPascal_r+1), 1);
+      for(int j = 1; j < i && j <= greatestPascal_r; ++j)
         pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
     }
-    greatestComputed_n = n;
+    greatestPascal_n = n;
   }
 
   /// The value at row n and column r of the triangle is now computed
